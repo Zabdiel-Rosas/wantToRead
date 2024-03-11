c39280e09c48673633a557a5c7e00c808e4b3801
zabdiel <zabdiel_rosas@outlook.com>
2024-03-11 06:51:23 -0600

    add(server.ts): app entry point is created

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
-console.log('Hi rogue programmer :)')
+import bodyParser from 'body-parser';
+import express from 'express';
+import { bookRouter } from './books/infrastructure/http/book-router';
+
+function bootstrap() {
+  const app = express();
+
+  app.use(bodyParser.json());
+  app.use('/api/books', bookRouter);
+
+  const port = 3000;
+
+  app.listen(port, () => {
+    console.log(`[APP] - Starting Application on port ${port}`);
+  });
+}
+
+bootstrap();
