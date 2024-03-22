28a39b369f4052e65781c8e40f3b134139225e89
zabdiel <zabdiel_rosas@outlook.com>
2024-03-22 10:18:19 -0600

    add(book-repository): update method is added to the interface

--- src/books/domain/book-repository.ts
+++ src/books/domain/book-repository.ts
@@ -4,4 +4,5 @@ export interface BookRepository {
   getAllBooks(): Promise<Book[] | null>;
   getBookById(id: string): Promise<Book | null>;
   createBook(id: string, name: string): Promise<Book | null>;
+  updateBook(id: string, name: string): Promise<Book | null>;
 }
