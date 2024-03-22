bb1f4125c1a9cb6d23c0cad7a1d01519765358fa
zabdiel <zabdiel_rosas@outlook.com>
2024-03-22 10:25:36 -0600

    feat(book-controller.ts): updateById method for handling the put request is added

--- src/books/infrastructure/http/book-controller.ts
+++ src/books/infrastructure/http/book-controller.ts
@@ -3,12 +3,14 @@ import { GetAllBooks } from '../../application/get-all-books';
 import { GetBookById } from '../../application/get-book-by-id';
 import { BookNotFound } from '../../domain/book-not-found';
 import { CreateBook } from '../../application/create-book';
+import { UpdateBook } from '../../application/update-book';
 
 export class BookController {
   constructor(
     private readonly getAllBooks: GetAllBooks,
     private readonly getBookById: GetBookById,
-    private readonly createBook: CreateBook
+    private readonly createBook: CreateBook,
+    private readonly updateBook: UpdateBook
   ) {}
 
   async readAll(_: Request, res: Response) {
@@ -41,6 +43,20 @@ export class BookController {
       const { id, name } = req.body;
       const newBook = await this.createBook.run(id, name);
       return res.status(201).send(newBook);
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(500).send(err.message);
+      }
+    }
+  }
+
+  async updateById(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const { name } = req.body;
+
+      const updatedBook = await this.updateBook.run(id, name);
+      return res.status(200).send(updatedBook);
     } catch (err) {
       if (err instanceof Error) {
         return res.status(500).send(err.message);
